39dbe5f0b4944bcacac7ac1c49e4f1c8eb19e4d9
Travis Johnson <travis_johnson@TXMAC140.local>
2013-09-06 10:11:46 -0500

    Intial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FarmBooks::Application.config.secret_key_base = 'ddd2ee368ca25597d387e1ff9b8b4a31e9dc274c006952dc489df6d07ad89fdb21e5344bf78bce2912ed4489641c03a8db8a87a0640ad608e3739665d4895f35'
