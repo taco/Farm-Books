6ae5bd0483ac6cc8672e58f00bcc4f103418d63c
Travis Johnson <tvjohnson@gmail.com>
2013-09-28 11:52:09 -0500

    update

--- server/application.js
+++ server/application.js
@@ -5,7 +5,7 @@ Meteor.startup(function () {
     Meteor.call('S3config', {
         key: process.env['S3_ACCESS'],
         secret: process.env['S3_SECRET'],
-        bucket: 'fhfarm-books'
+        bucket: 'fhfarm-test1'
     });
 
 Meteor.publish('transactions', function (id) {
@@ -17,5 +17,9 @@ Meteor.publish('transactions', function (id) {
 Meteor.methods({
     s3Complete:function(url, context){
         console.log('Add '+url+' to the id of '+context);
+    },
+    nodeVersion: function () {
+        console.log('NODE-VERSION', process.version);
+        return process.version;
     }
 });
